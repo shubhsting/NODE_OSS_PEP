af807ffb2d16d536a97d8077f49d43203b729c2e
Shubham <shubhamsingh1840@gmail.com>
2020-12-09 13:42:48 +0530

    updated

--- Lec7_Extensions/facts/contentScript.js
+++ Lec7_Extensions/facts/contentScript.js
@@ -1,12 +1,9 @@
-let img = document.querySelectorAll("img");
 
 var imgURL = chrome.runtime.getURL("./images/clearall.png");
 
-
-
-
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
+        let img = document.querySelectorAll("img");
         for (let i = 0; i < img.length; i++) {
             img[i].src = imgURL;
         }
